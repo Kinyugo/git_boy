366bba51f959b4164a42e097c6602b352b60b0d9
Kinyugo <kinyugokyde@gmail.com>
2020-06-05 20:43:13 +0300

    Refactored 'repostory_live.ex' and 'epository_live.html.eex'

--- lib/git_boy_web/live/repository_live/repository_live.ex
+++ lib/git_boy_web/live/repository_live/repository_live.ex
@@ -1,42 +1,136 @@
 defmodule GitBoyWeb.RepositoryLive do
   use GitBoyWeb, :live_view
-  alias GitBoy.Repositories
-  alias GitBoy.ProgrammingLanguages
-  alias GitBoy.Licenses
-  alias GitBoy.Licenses.License
   alias GitBoyWeb.RepositoryLive.RepoComponent
+  alias GitBoy.Repositories
+  alias GitBoy.Licenses
+  alias GitBoy.ProgrammingLanguages
   require Logger
 
   @impl true
   def mount(_params, _session, socket) do
-    repo_assigns = get_repo_assigns(socket)
+    Logger.debug("Running mount in process: #{inspect(self())}")
 
+    {repo_server, default_repos} = setup_repo_server(socket)
     common_licenses = Licenses.list_licenses()
 
-    default_assigns =
-      [
-        repo_name: "",
-        language: "",
-        language_suggestions: [],
-        license: "",
-        sort: "",
-        common_licenses: common_licenses,
-        loading: false
-      ] ++ repo_assigns
+    default_assigns = [
+      repo_server: repo_server,
+      repos: default_repos,
+      common_licenses: common_licenses,
+      language_suggestions: [],
+      repo_name: "",
+      language: "",
+      license: "",
+      sort_order: "",
+      loading: false
+    ]
 
     socket = assign(socket, default_assigns)
 
     {:ok, socket, temporary_assigns: [repos: [], language_suggestions: []]}
   end
 
+  defp setup_repo_server(socket) do
+    case connected?(socket) do
+      true ->
+        # Start repo server
+        {:ok, repo_server} = Repositories.start_link([])
+        # Fetch default list of repositories
+        default_repos = Repositories.fetch_repositories(repo_server)
+
+        {repo_server, default_repos}
+
+      false ->
+        {nil, []}
+    end
+  end
+
   @impl true
-  def handle_event("apply_all_filters", _params, socket) do
+  def handle_event("search_repositories", params, socket) do
+    %{
+      "language" => language,
+      "repo_name" => repo_name,
+      "license" => license,
+      "sort_order" => sort_order
+    } = params
+
+    attrs = [
+      repos: [],
+      loading: true,
+      language: language,
+      repo_name: repo_name,
+      license: license,
+      sort_order: sort_order
+    ]
+
+    send(
+      self(),
+      {:search_repositories, Keyword.take(attrs, [:language, :repo_name, :license, :sort_order])}
+    )
+
+    {:noreply, assign(socket, attrs)}
+  end
+
+  @impl true
+  def handle_event("update_language_suggestions", %{"value" => language_prefix}, socket) do
+    repos =
+      socket
+      |> get_repo_server()
+      |> Repositories.filter_by_language(language_prefix)
+
+    language_suggestions = ProgrammingLanguages.suggest_languages(language_prefix)
+
+    attrs = [language_suggestions: language_suggestions, repos: repos, language: language_prefix]
+
+    {:noreply, assign(socket, attrs)}
+  end
+
+  @impl true
+  def handle_event("filter_repos_by_name", %{"value" => name_prefix}, socket) do
+    repos =
+      socket
+      |> get_repo_server()
+      |> Repositories.filter_by_name(name_prefix)
+
+    attrs = [repos: repos, repo_name: name_prefix]
+
+    {:noreply, assign(socket, attrs)}
+  end
+
+  @impl true
+  def handle_event("filter_repos_by_license", %{"value" => license}, socket) do
+    repos =
+      socket
+      |> get_repo_server()
+      |> Repositories.filter_by_license(license)
+
+    attrs = [repos: repos, license: license]
+
+    {:noreply, assign(socket, attrs)}
+  end
+
+  @impl true
+  def handle_event("sort_repos", %{"value" => sort_order}, socket) do
+    [sort_by, order] = parse_sort_order(sort_order)
+
+    repos =
+      socket
+      |> get_repo_server()
+      |> Repositories.sort_repos(sort_by, order)
+
+    attrs = [repos: repos, sort_order: sort_order]
+
+    {:noreply, assign(socket, attrs)}
+  end
+
+  @impl true
+  def handle_event("filter_and_sort_repos", _params, socket) do
     language = get_assigns(socket, :language)
     repo_name = get_assigns(socket, :repo_name)
     license = get_assigns(socket, :license)
-    sort_order = get_assigns(socket, :sort)
+    sort_order = get_assigns(socket, :sort_order)
 
-    [sort_by, order] = parse_sort(sort_order)
+    [sort_by, order] = parse_sort_order(sort_order)
 
     filters = [
       language: language,
@@ -46,149 +140,47 @@ defmodule GitBoyWeb.RepositoryLive do
       order: order
     ]
 
-    Logger.info("Apply multiple filters to repos: #{inspect(filters)}")
+    repos =
+      socket
+      |> get_repo_server()
+      |> Repositories.apply_all_filters(filters)
 
-    repos = Repositories.apply_all_filters(get_assigns(socket, :repo_server), filters)
+    attrs = [repos: repos]
 
-    {:noreply, assign(socket, repos: repos)}
+    {:noreply, assign(socket, attrs)}
   end
 
   @impl true
-  def handle_event("sort_repos", %{"value" => sort_order}, socket) do
-    Logger.info("Sorting repositories by: #{inspect(sort_order)}")
+  def handle_info({:search_repositories, params}, socket) do
+    [language, repo_name, license, sort_order] = Keyword.values(params)
 
-    [sort_by, order] = parse_sort(sort_order)
-
-    sorted_repos = Repositories.sort_repos(get_assigns(socket, :repo_server), sort_by, order)
-
-    {:noreply, assign(socket, repos: sorted_repos, sort: sort_order)}
-  end
-
-  @impl true
-  def handle_event("filter_repos_by_license", %{"value" => license_key} = params, socket) do
-    Logger.info("Filtering repositories by license: #{inspect(params)}")
-
-    repos = Repositories.filter_by_license(get_assigns(socket, :repo_server), license_key)
-
-    {:noreply, assign(socket, repos: repos, license: license_key)}
-  end
-
-  @impl true
-  def handle_event("filter_repos_by_name", %{"value" => repo_name_prefix} = params, socket) do
-    Logger.info("Filtering repositories by name: #{inspect(params)}")
-
-    repos = Repositories.filter_by_name(get_assigns(socket, :repo_server), repo_name_prefix)
-
-    {:noreply, assign(socket, repos: repos, repo_name: repo_name_prefix)}
-  end
-
-  @impl true
-  def handle_event(
-        "search_repositories",
-        %{"language" => language, "repo_name" => repo_name, "license" => license, "sort" => sort},
-        socket
-      ) do
-    send(
-      self(),
-      {:search_repositories,
-       [language: language, repo_name: repo_name, license: license, sort: sort]}
-    )
-
-    {:noreply,
-     assign(socket,
-       repos: [],
-       language: language,
-       repo_name: repo_name,
-       license: license,
-       sort: sort,
-       loading: true
-     )}
-  end
-
-  @impl true
-  def handle_event(
-        "update_assigns",
-        %{"language" => language, "repo_name" => repo_name} = params,
-        socket
-      ) do
-    Logger.info("Updating assigns: #{inspect(params)}")
-    {:noreply, assign(socket, language: language, repo_name: repo_name)}
-  end
-
-  @impl true
-  def handle_event(
-        "update_language_suggestions_and_filter_repos",
-        %{"value" => language_prefix} = params,
-        socket
-      ) do
-    repos = Repositories.filter_by_language(get_assigns(socket, :repo_server), language_prefix)
-    language_suggestion = ProgrammingLanguages.suggest_languages(language_prefix)
-
-    socket =
-      assign(socket,
-        language_suggestions: language_suggestion,
-        repos: repos,
-        language: language_prefix
-      )
-
-    Logger.info("Updating language suggestions: #{inspect(params)}")
-
-    {:noreply, socket}
-  end
-
-  ## Info
-  @impl true
-  def handle_info(
-        {:search_repositories,
-         [language: language, repo_name: repo_name, license: license, sort: sort]},
-        socket
-      ) do
-    [sort_by, order] = parse_sort(sort)
+    [sort_by, order] = parse_sort_order(sort_order)
+    query = [repo_name, "language:#{language}", "license:#{license}"]
 
     query_params = [
-      query: [repo_name, "language:#{language}", "license:#{license}"],
+      query: query,
       sort: sort_by,
       order: order
     ]
 
-    repos = Repositories.fetch_repositories(get_assigns(socket, :repo_server), query_params)
+    repos =
+      socket
+      |> get_repo_server()
+      |> Repositories.fetch_repositories(query_params)
 
     {:noreply, assign(socket, repos: repos, loading: false)}
   end
 
-  ## Helpers
-  defp parse_sort(""), do: ["", ""]
+  defp parse_sort_order(""), do: ["", ""]
 
-  defp parse_sort(sort) do
-    sort
+  defp parse_sort_order(sort_order) do
+    sort_order
     |> String.split()
+    |> Enum.slice(0, 2)
   end
 
-  def sort_options do
-    [
-      "Sort By...": "",
-      "Stars Ascending": "stars asc",
-      "Stars Desending": "stars desc",
-      "Forks Ascending": "forks asc",
-      "Forks Descending": "forks desc",
-      "Issues Ascending": "issues asc",
-      "Issues Descending": "issues desc"
-    ]
-  end
-
-  def render_license_option(assigns, selected_license) do
-    assigns = Enum.into(assigns, %{})
-
-    ~L"""
-      <option
-      value="<%= @license.key %>"
-      class = "repositories__license__option <%= if @in_repo_licenses?, do: 'repositories__license__option--in-licenses' %>"
-      <%= if @license.key == selected_license, do: "selected" %>
-      >
-        <%= @license.name %>
-      </option>
-    """
-  end
+  defp get_repo_server(socket), do: get_assigns(socket, :repo_server)
+  defp get_assigns(socket, key), do: Map.get(socket.assigns, key)
 
   def list_licenses(nil, common_licenses) do
     Enum.map(common_licenses, fn license -> [license: license, in_repo_licenses?: false] end)
@@ -210,21 +202,29 @@ defmodule GitBoyWeb.RepositoryLive do
     end)
   end
 
-  defp get_assigns(socket, key), do: Map.get(socket.assigns, key)
+  def render_license_option(assigns, selected_license) do
+    assigns = Enum.into(assigns, %{})
 
-  defp get_repo_assigns(socket) do
-    if connected?(socket) do
-      # Start repositories server
-      {:ok, repo_server} = Repositories.start_link([])
-      # Fetch default list of repositories
-      repos = Repositories.fetch_repositories(repo_server)
+    ~L"""
+      <option
+      value="<%= @license.key %>"
+      class = "repositories__license__option <%= if @in_repo_licenses?, do: 'repositories__license__option--in-licenses' %>"
+      <%= if @license.key == selected_license, do: "selected" %>
+      >
+        <%= @license.name %>
+      </option>
+    """
+  end
 
-      [repo_server: repo_server, repos: repos]
-    else
-      [
-        repo_server: nil,
-        repos: []
-      ]
-    end
+  def sort_options() do
+    [
+      "Sort By": "",
+      "Stars Ascending": "stars asc",
+      "Stars Desending": "stars desc",
+      "Forks Ascending": "forks asc",
+      "Forks Descending": "forks desc",
+      "Issues Ascending": "issues asc",
+      "Issues Descending": "issues desc"
+    ]
   end
 end
